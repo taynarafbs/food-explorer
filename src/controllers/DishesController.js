debe6346332d64a7c7d54ae21045a85bc6fdee7d
Taynara <tfbsantos@gmail.com>
2024-01-09 21:50:06 -0300

    09.01

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -3,7 +3,7 @@ const knex = require("../database/knex");
 class DishesController {
     async create(request, response) {
         const { title, description, price, type, ingredients } = request.body;
-        const { user_id } = request.params;
+        const user_id = request.user.id;
 
         const [dish_id] = await knex("dishes").insert({
             title,
@@ -47,7 +47,9 @@ class DishesController {
     };
 
     async index(request, response) {
-        const { title, user_id, ingredients } = request.query;
+        const { title, ingredients } = request.query;
+
+        const user_id = request.user.id;
 
         let dishes;
 

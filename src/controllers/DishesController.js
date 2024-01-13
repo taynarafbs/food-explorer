60843e9569496d05661f6bcff2f50beb1e2360b9
Taynara <tfbsantos@gmail.com>
2024-01-12 21:31:44 -0300

    12.01

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -1,16 +1,32 @@
 const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const DiskStorage = require("../providers/DiskStorage");
 
 class DishesController {
     async create(request, response) {
         const { title, description, price, type, ingredients } = request.body;
         const user_id = request.user.id;
 
+        const diskStorage = new DiskStorage;
+
+        if (!title || !price || !description || !type || !ingredients) {
+            throw new AppError('Não foi possivel realizar o cadastro do prato.')
+        }
+        const user = await knex("users").where({ id: user_id }).first();
+        if (!user) {
+            throw new AppError("Usuário não autorizado", 401);
+        }
+
+        // const filename = img && await diskStorage.saveFile(img.filename);
+
+
         const [dish_id] = await knex("dishes").insert({
             title,
             description,
             price,
             type,
-            user_id
+            user_id,
+            // img: filename
         });
 
         const ingredientsInsert = ingredients.map(name => {
@@ -23,6 +39,64 @@ class DishesController {
         await knex("ingredients").insert(ingredientsInsert);
 
         return response.json();
+    };
+
+    async update(request, response) {
+        const { title, description, price, type, ingredients } = request.body;
+        const user_id = request.user.id;
+
+        const { id } = request.params;
+        let img = null;
+        let filename = null;
+
+        const diskStorage = new DiskStorage();
+
+        const user = await knex("users").where({ id: user_id }).first();
+        if (!user) {
+            throw new AppError("Somente usuários administradores", 401);
+        }
+
+        const dish = await knex("dishes").where({ id }).first();
+
+        filename = dish.img || null;
+
+        if (request.file) {
+            if (filename) {
+                await diskStorage.deleteFile(filename)
+            }
+
+            img = request.file.filename
+            console.log(img)
+            if (img) {
+                filename = await diskStorage.saveFile(img);
+            }
+        }
+
+        const newDish = {
+            ...dish,
+            title,
+            description,
+            price,
+            type,
+            img: filename
+        }
+
+        const responseDishes = await knex("dishes").where({ id }).update(newDish);
+
+        if (ingredients) {
+            const newIngredientsInsert = ingredients.map(name => {
+                return {
+                    dish_id: id,
+                    name,
+                };
+            });
+            await knex("ingredients").where({ dish_id: id }).delete();
+            await knex("ingredients").insert(newIngredientsInsert);
+        }
+
+
+        return response.json(responseDishes);
+
     };
 
     async show(request, response) {
@@ -79,6 +153,8 @@ class DishesController {
 
         return response.json(dishes);
     };
+
+
 
 };
 
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const DishesController = require("../controllers/DishesController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
 const dishesRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const dishesController = new DishesController();
 
 dishesRoutes.use(ensureAuthenticated);
-dishesRoutes.post("/", dishesController.create);
+
+dishesRoutes.post("/", ensureAuthenticated, upload.single("img"), dishesController.create);
+dishesRoutes.put("/:id", ensureAuthenticated, upload.single("img"), dishesController.update);
+
 dishesRoutes.get("/", dishesController.index);
 dishesRoutes.get("/:id", dishesController.show);
 dishesRoutes.delete("/:id", dishesController.delete);

3c19cd9b1a6e4c0670006a1fbcb61de782bd3728
Taynara <tfbsantos@gmail.com>
2024-01-18 15:47:17 -0300

    16.01

--- src/server.js
+++ src/server.js
@@ -3,18 +3,18 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
 
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
-
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
-
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
